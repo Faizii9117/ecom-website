0ba2fac363583d8c6223fd603c19e7e43b959ea4
Faizii9117 <faisalshaikh9117@gmail.com>
2024-10-20 23:39:30 +0530

    setting changed

--- ecom/settings.py
+++ ecom/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecom_webpage', 
+        'USER': 'postgres',
+        'PASSWORD': 'ghost786143',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
