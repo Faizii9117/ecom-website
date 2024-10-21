1b6ed9013d007d436eba408e1dc572478b9857c6
farhan-khan <farhankhan2004f@gmail.com>
2024-10-21 13:12:58 +0530

    changes

--- ecom/settings.py
+++ ecom/settings.py
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': 
+    'default':{ 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ecom_webpage', 
         'USER': 'postgres',
         'PASSWORD': 'ghost786143',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
+    }
 }
 
 
